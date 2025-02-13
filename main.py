3179545245e42b58a3afaa0076d6f393bf8f6ad4
SaidaliOtaboyev <161677329+SaidaliOtaboyev@users.noreply.github.com>
2025-02-13 20:29:58 +0500

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#saidali
 import random
 from task1 import kwargsAcceptFun
 from task2 import typeBasedTransformer
@@ -10,16 +11,22 @@ def func():
     n = random.randint(10, 751)
     for i in range(n):
         result += (i ** 2)
+    return result 
 
 @decorator_1
-def funx(n=2, m=5):
+def funx(n=None):
     print("I am ready to do serious stuff")
     max_val = float('-inf')
-    n = random.randint(10, 751)
+    
+  
+    if n is None:
+        n = random.randint(10, 751)
+
     res = [pow(i, 2) for i in range(n)]
     for i in res:
         if i > max_val:
             max_val = i
+    return max_val 
 
 if __name__ == "__main__":
     func()
@@ -27,4 +34,3 @@ if __name__ == "__main__":
     func()
     funx()
     func()
-
