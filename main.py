497d426ad886503d5d5f7471bfd46d68e8bc1184
SaidaliOtaboyev <161677329+SaidaliOtaboyev@users.noreply.github.com>
2025-02-13 21:45:33 +0500

    main.py

--- main.py
+++ main.py
@@ -7,25 +7,25 @@ from task3 import decorator_1
 @decorator_1
 def func():
     print("I am ready to Start")
-    result = 0
+    gonetime = 0
     n = random.randint(10, 751)
     for i in range(n):
-        result += (i ** 2)
-    return result 
+         gonetime += (i ** 2)
+    return  gonetime 
 
 @decorator_1
 def funx(n=None):
     print("I am ready to do serious stuff")
-    max_val = float('-inf')
+    uzbeksila = float('-inf')
 
     if n is None:
         n = random.randint(10, 751)
 
     res = [pow(i, 2) for i in range(n)]
     for i in res:
-        if i > max_val:
-            max_val = i
-    return max_val 
+        if i > uzbeksila:
+            uzbeksila = i
+    return uzbeksila 
 
 if __name__ == "__main__":
     func()
