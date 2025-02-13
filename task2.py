d591de04af4ffabe2b1bec110e64b4d6bcc7be23
SaidaliOtaboyev <161677329+SaidaliOtaboyev@users.noreply.github.com>
2025-02-13 20:02:50 +0500

    task2.py

--- task2.py
+++ task2.py
@@ -1,16 +1,23 @@
-def typeBasedTransformer(**kwargs):
-    transformed = {}
-    for key, value in kwargs.items():
-        if isinstance(value, (int, float)):
-            transformed[key] = value ** 2
-        elif isinstance(value, str):
-            transformed[key] = value[::-1]
-        elif isinstance(value, bool):
-            transformed[key] = not value
-        elif isinstance(value, (list, tuple)):
-            transformed[key] = value[::-1]
-        elif isinstance(value, dict):
-            transformed[key] = {v: k for k, v in value.items()}
+#Saidali
+def typeBasedTransformer(**argumentdetails):
+    converteddata = {} 
+    
+    for identifier, data in argumentdetails.items():  
+        if isinstance(data, (int, float)):
+            converteddata[identifier] = data ** 2  
+            
+        elif isinstance(data, str):
+            converteddata[identifier] = data[::-1]  
+            
+        elif isinstance(data, bool):
+            converteddata[identifier] = not bool(data)            
+        elif isinstance(data, (list, tuple)):
+            converteddata[identifier] = data[::-1]  
+            
+        elif isinstance(data, dict):
+            converteddata[identifier] = {v: k for k, v in data.items()}  
+            
         else:
-            transformed[key] = value
-    return transformed
+            converteddata[identifier] = data  
+    
+    return converteddata  
