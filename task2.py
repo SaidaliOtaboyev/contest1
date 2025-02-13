2d4eb91e57d5b6c44b8f16fd4bffc685dfeda079
SaidaliOtaboyev <161677329+SaidaliOtaboyev@users.noreply.github.com>
2025-02-13 19:34:06 +0500

    task2.py

--- task2.py
+++ task2.py
@@ -1,18 +1,16 @@
 def typeBasedTransformer(**kwargs):
-    transformed_data = {}
-
+    transformed = {}
     for key, value in kwargs.items():
-        if isinstance(value, (int, float)):  # Square numbers
-            transformed_data[key] = value ** 2
-        elif isinstance(value, str):  # Reverse strings
-            transformed_data[key] = value[::-1]
-        elif isinstance(value, bool):  # Invert booleans
-            transformed_data[key] = not value
-        elif isinstance(value, (list, tuple)):  # Reverse lists & tuples
-            transformed_data[key] = value[::-1]
-        elif isinstance(value, dict):  # Swap keys & values (assuming unique values)
-            transformed_data[key] = {v: k for k, v in value.items()}
-        else:  # Keep other types unchanged
-            transformed_data[key] = value
-
-    return transformed_data
+        if isinstance(value, (int, float)):
+            transformed[key] = value ** 2
+        elif isinstance(value, str):
+            transformed[key] = value[::-1]
+        elif isinstance(value, bool):
+            transformed[key] = not value
+        elif isinstance(value, (list, tuple)):
+            transformed[key] = value[::-1]
+        elif isinstance(value, dict):
+            transformed[key] = {v: k for k, v in value.items()}
+        else:
+            transformed[key] = value
+    return transformed
