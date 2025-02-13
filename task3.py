413ceaf29a6101b0bca5e165ce529a8ce24c2830
SaidaliOtaboyev <161677329+SaidaliOtaboyev@users.noreply.github.com>
2025-02-13 20:13:20 +0500

    task3.py

--- task3.py
+++ task3.py
@@ -1,11 +1,12 @@
+#saidali
 import time
 
 def decorator_1(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        execution_time = end_time - start_time
-        print(f"{func.__name__} call executed in {execution_time:.4f} sec")
+    def measure_runningtime(*values, **named_inputs):
+        initial_time = time.time() 
+        result = func(*values, **named_inputs)  
+        recorded_time = time.time()  
+        duration = recorded_time - initial_time 
+        print(f"{func.__name__} call executed in {duration:.4f} sec") 
         return result
-    return wrapper
+    return measure_runningtime
